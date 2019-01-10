5054914356cf1847f0638fcf4eb232042b0da483
Jeff Borda <jeffborda@outlook.com>
2019-01-10 11:40:18 -0800

    style updates

--- app/src/main/java/com/example/health_tracker/MainActivity.java
+++ app/src/main/java/com/example/health_tracker/MainActivity.java
@@ -19,9 +19,7 @@ import java.util.TimerTask;
 public class MainActivity extends AppCompatActivity {
 
 
-    public long startTime = 0;
-    public boolean isRunning = false;
-    TimerTask task;
+
     // Image Carousel
     CarouselView carouselView;
     int[] sampleImages = {R.drawable.exercise1, R.drawable.exercise2, R.drawable.exercise3};
@@ -53,66 +51,9 @@ public class MainActivity extends AppCompatActivity {
         startActivity(fingerExerciseIntent);
     }
 
-    public void onStopwatchStart(View v) {
-        if(isRunning) {
-            return;
-        }
-        isRunning = true;
-        Timer stopwatch = new Timer();
-        startTime = System.currentTimeMillis();
-
-        int delay = 0; // Delay in milliseconds
-        int period = 10; // Period in milliseconds
-
-
-        task = new TimerTask() {
-            @Override
-            public void run() {
-                TextView textView = findViewById(R.id.stopwatch_text);
-                textView.setText(setStopwatch());
-            }
-        };
-        stopwatch.scheduleAtFixedRate(task, delay, period);
-
-    }
-
-    public void onStopwatchStop(View v) {
-        isRunning = false;
-        task.cancel();
-    }
-
-    public void onStopwatchReset(View v) {
-        if(isRunning) {
-            task.cancel();
-        }
-        TextView textView = findViewById(R.id.stopwatch_text);
-        textView.setText("0:00:00.000");
-
-        if(isRunning) {
-            Timer stopwatch = new Timer();
-            startTime = System.currentTimeMillis();
-
-            int delay = 0; // Delay in milliseconds
-            int period = 10; // Period in milliseconds
-
-
-            task = new TimerTask() {
-                @Override
-                public void run() {
-                    TextView textView = findViewById(R.id.stopwatch_text);
-                    textView.setText(setStopwatch());
-                }
-            };
-            stopwatch.scheduleAtFixedRate(task, delay, period);
-        }
-    }
-
-    public String setStopwatch() {
-        long nowMilis = System.currentTimeMillis();
-        long diff = nowMilis - startTime - (16*60*60*1000); // Time zone adjust
-        Date date = new Date(diff);
-        SimpleDateFormat timeFormat = new SimpleDateFormat("H:mm:ss.SSS");
-        return timeFormat.format(date);
+    public void goToStopwatch(View v) {
+        Intent stopwatchIntent = new Intent(this, StopWatch.class);
+        startActivity(stopwatchIntent);
     }
 
 
